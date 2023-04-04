1d6a2eae263e8068e687bfafd111796cd0e175fd
Hassan Jamil <hassan@curv.ai>
2023-04-04 19:44:28 -0400

    ig for now lets keep this change?? idk lol

--- state.h
+++ state.h
@@ -1,7 +1,11 @@
 #ifndef STATE_H
 #define STATE_H
 
-enum class StateType{Purchase, Mortgage, Trade, Unmortgage, Bankrupt, Move};
+// When it is players turn, we can call one of the methods in Player class (which aiden implementing), 
+// which will create a State object to characterize what kind of 
+
+
+enum class StateType{Purchase, Mortgage, Trade, Unmortgage, Bankrupt, PrevPosn};
 
 class State{
     StateType type;
