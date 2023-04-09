ee3fa0de779cc75130a7b221bf9f2aaf35935c83
Hassan Jamil <hassan@curv.ai>
2023-04-09 13:54:41 -0400

    copied textdisplay and board changes from ahmed-2 branch

--- board.h
+++ board.h
@@ -2,7 +2,6 @@
 #define BOARD_H
 #include <vector>
 #include <memory>
-#include <string>
 
 class TextDisplay;
 class Player;
@@ -20,7 +19,7 @@ class Board {
 
     std::vector<std::shared_ptr<Player>> playerList;
     //TextDisplay *td = nullptr; // The text display.
-    std::shared_ptr<TextDisplay> td; // The text display
+    TextDisplay *td; // The text display
     std::vector<std::shared_ptr<Cell>> Cells; // The list of cells on the board
 
     char pieceSymbol(std::string pieceName);
